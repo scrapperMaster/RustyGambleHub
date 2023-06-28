bb23e487a836bc43acf3b6f536abac6d93a1102c
YARD DEPARTAMENT <ivankochettov@gmail.com>
2023-06-28 18:01:11 +0300

    Add SEO and settings for prod

--- RustyGambleHub/settings_prod.py
+++ RustyGambleHub/settings_prod.py
@@ -23,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xx6ru5-^rmqzk0l#h32+)=)=p^pg!594h_!upmp+jdepvj65tf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    '*'
+    'rustygamble.com'
+    'www.rustygamble.com'
 ]
 
 
@@ -116,16 +117,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = 'static/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
+from django.conf.urls.static import static
 
+from RustyGambleHub import settings
 from main import views
 
 urlpatterns = [
     path('', views.join_discord)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
