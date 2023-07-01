c1f5acc352d50a9ca10e30622c19f7a745343399
xialesss <zzenplay@gmail.com>
2023-07-01 14:50:08 +0300

    тестовые карточки аниме на реакте

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import AnimeCard from "./AnimeCard";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AnimeCard title="Naruto" year="2002" genre="Comedy"></AnimeCard>
+      <AnimeCard title="Berserk" year="1996" genre="Dramm"></AnimeCard>
     </div>
   );
 }
