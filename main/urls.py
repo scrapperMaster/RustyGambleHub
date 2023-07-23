276cc20166be45fbb78caacd29d14d252d671251
YARD DEPARTAMENT <ivankochettov@gmail.com>
2023-07-23 13:55:25 +0300

    Фикс дискорда (фикс url)

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from django.conf.urls.static import static
 from RustyGambleHub import settings
-from rest_framework import routers
 
 from django.urls import path
 
@@ -10,13 +9,10 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-                  path('get_steam_auth_url/', views.get_steam_auth_url, name='get_steam_auth_url'),
-                  path('login/', views.login, name='login'),
-                  path('auth/login/steam/', views.login_done, name='login_done'),
-                  path('logout/', views.logout, name='logout'),
-                  path('protected/', views.protected, name='protected'),
-                  path('error/', views.error, name='error'),
-                  path('user_data/', views.get_user_data, name='get_user_data'),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('steam_login/', views.steam_login, name='steam_login'),
+    path('processlogin/', views.steam_authenticate, name='process_steam_login'),
+    path('user_data/', views.get_user_data, name='get_user_data'),
+    path('logout/', views.logout_view, name='logout'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
